b782f4dc70d508a86bc172a50f792d3834d33f2b
nickhwite <bbyicnka@yandex.ru>
2024-03-07 10:08:37 +0300

    refact(refact): Поправил все компоненты

--- src/components/SearchResults/SearchResults.tsx
+++ src/components/SearchResults/SearchResults.tsx
@@ -1,16 +1,16 @@
-import { useContext } from "react";
-import { SearchContext } from "./SearchContext";
+import { FC, useContext } from "react";
 import { UserCard } from "../UserCard/UserCard";
-
-import "./style.css";
-
-export function SearchResults() {
+import { SearchContext } from "./helpers";
+import s from './SearchResults.module.css'
+import { IUserInterface } from "../../types/user.interface.ts";
+export const SearchResults: FC = () => {
   const { users } = useContext(SearchContext);
-
   return (
-    <div className="usersList">
-      {users.map((user) => (
-        <UserCard {...user} />
+    <div className={s.users_list}>
+      {users.map((user:IUserInterface) => (
+          <li className={s.user__li} key={user.id}>
+            <UserCard {...user} />
+          </li>
       ))}
     </div>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': 'https://dummyjson.com/users/search?q=',
+    },
+  },
+  preview: {
+    port: 80,
+  },
 })
